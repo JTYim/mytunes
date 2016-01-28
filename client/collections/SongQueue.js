a5ba132b0cf665f66ece7c0a9558d016ae35d911
JT Yim <JT@JTYim.com>
2016-01-28 02:57:33 -0500

    songQueue = new collection ?

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,11 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
-  }
+	initialize: function(){
+		this.trigger('enqueue', this)
+	},
 
+	enqueue: function(){
+		this.trigger('enqueue', this)				
+	}
 });
--- client/views/AppView.js
+++ client/views/AppView.js
@@ -1,22 +1,23 @@
 // AppView.js - Defines a backbone view class for the whole music app.
 var AppView = Backbone.View.extend({
 
-  initialize: function(params){
-    this.playerView = new PlayerView({model: this.model.get('currentSong')});
-    this.libraryView = new LibraryView({collection: this.model.get('library')});
+	initialize: function(params){
+		this.playerView = new PlayerView({model: this.model.get('currentSong')});
+		this.libraryView = new LibraryView({collection: this.model.get('library')});
+		this.songQueueView = new LibraryView({collection: this.model.set('library')});
+		// change:currentSong - this is Backbone's way of allowing you to filter events to
+		// ONLY receive change events for the specific property, 'currentSong'
+		this.model.on('change:currentSong', function(model){
+			this.playerView.setSong(model.get('currentSong'));
+		}, this);
+	},
 
-    // change:currentSong - this is Backbone's way of allowing you to filter events to
-    // ONLY receive change events for the specific property, 'currentSong'
-    this.model.on('change:currentSong', function(model){
-      this.playerView.setSong(model.get('currentSong'));
-    }, this);
-  },
-
-  render: function(){
-    return this.$el.html([
-      this.playerView.$el,
-      this.libraryView.$el
-    ]);
-  }
+	render: function(){
+		return this.$el.html([
+			this.playerView.$el,
+			this.libraryView.$el,
+			this.songQueueView.$el
+		]);
+	}
 
 });
